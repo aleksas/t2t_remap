9db2bf8918226fb58cf2a455630b4ee08ddc0b64
Aleksas Pielikis <ant.kampo@gmail.com>
2019-10-27 22:10:48 +0000

    cleanup

--- serve.sh
+++ serve.sh
@@ -9,12 +9,6 @@ DATA_DIR=$HOME/t2t_data
 TMP_DIR=/tmp/t2t_datagen
 TRAIN_DIR=$HOME/t2t_train/$PROBLEM/$MODEL-$HPARAMS_SET
 
-#curl -o ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-#unzip ngrok.zip
-#nohup ./ngrok http 8888 &
-#sleep 3
-#curl http://127.0.0.1:4040/api/tunnels
-
 tensorflow_model_server \
   --port=8888 \
   --model_name=$MODEL \
